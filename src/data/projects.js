7f4636f43a7648fa59b3acebefccf433c4fb62f4
harmeet651 <harmeet651@gmail.com>
2019-11-15 05:01:16 -0800

    project, resume added

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        title: 'Blog using Django ',
+        url: 'https://github.com/harmeet651/Django__Project',
+        image: 'django.png',
+        description: 'Main developer, created in Datumize. Typescript library to capture interactions of web visitors without altering the code of the website, and Big Data Platform to extract analytics from such data.',
+        prize: 'Maximum Score with honors in my Master Thesis',
+        tags: {
+            types: ['big data'],
+            languages: ['typescript','java'],
+            libraries: ['spark','hbase','elasticsearch'],
+            others: ['Master Thesis']
+
+        }
+    },
+    {
+        title: 'uConverter',
+        url: 'https://play.google.com/store/apps/details?id=com.ismaro3.conversor',
+        image: 'conversor.png',
+        description: 'Simple, easy to use and complete Android app that helps you converting measuring units and currencies. Downloaded by 20,000 people around the world.',
+        prize: '',
+        tags: {
+            types: ['android app'],
+            languages: ['java'],
+            libraries: [],
+            others: []
+
+        }
+    }
+ ];
+
+ export default projects;
