ab12530ad78ac1caaaec3d8719814be89c18d0d2
harmeet651 <harmeet651@gmail.com>
2019-11-15 07:29:11 -0800

    projects added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,32 +1,121 @@
 const projects = [
     {
-        title: 'Blog using Django ',
+        title: 'Blog Application',
         url: 'https://github.com/harmeet651/Django__Project',
         image: 'django.png',
-        description: 'Main developer, created in Datumize. Typescript library to capture interactions of web visitors without altering the code of the website, and Big Data Platform to extract analytics from such data.',
-        prize: 'Maximum Score with honors in my Master Thesis',
+        description: 'Responsive blog application, where you can sign up and start blogging',
+        prize: '',
         tags: {
-            types: ['big data'],
-            languages: ['typescript','java'],
-            libraries: ['spark','hbase','elasticsearch'],
-            others: ['Master Thesis']
-
+            types: ['Web App'],
+            languages: ['pyhton','javascript','html','css'],
+            libraries: ['bootstrap','django'],
+            others: ['']
         }
     },
     {
-        title: 'uConverter',
-        url: 'https://play.google.com/store/apps/details?id=com.ismaro3.conversor',
-        image: 'conversor.png',
-        description: 'Simple, easy to use and complete Android app that helps you converting measuring units and currencies. Downloaded by 20,000 people around the world.',
+        title: 'Find any Place',
+        url: 'https://github.com/harmeet651/FindPlaces',
+        image: 'findplace.png',
+        description: 'Look for any places, address, routes, pictures of the place with Google and Yelp reviews',
         prize: '',
         tags: {
-            types: ['android app'],
-            languages: ['java'],
-            libraries: [],
-            others: []
-
+            types: ['Web App'],
+            languages: ['javascript','html','css'],
+            libraries: ['express','nodejs','bootstrap','jquery'],
+            others: ['ajax']
         }
-    }
+    },
+    {
+        title: 'Rental Cars',
+        url: 'https://github.com/harmeet651/RentalCars',
+        image: 'rentalcars.png',
+        description: 'ASP .Net (MVC) application for a manager to manage the customers and cars of the store he/she owns',
+        prize: '',
+        tags: {
+            types: ['Web App'],
+            languages: ['c#','html','css'],
+            libraries: ['asp .net','','bootstrap','jquery', 'ado .net'],
+            others: ['sql server']
+        }
+    },
+    {
+        title: 'Weather Teller',
+        url: 'https://github.com/harmeet651/WeatherApp',
+        image: 'weatherapp.png',
+        description: 'App to get weather and forcast information of any city',
+        prize: '',
+        tags: {
+            types: ['Web App'],
+            languages: ['html','css', 'javascript'],
+            libraries: ['express','nodejs','bootstrap','jquery'],
+            others: ['ajax']
+        }
+    },
+    {
+        title: 'WordPlay VR',
+        url: 'https://github.com/harmeet651/WordPlay-Vr-Site',
+        image: 'wordplay.png',
+        description: 'Scalable, virtual reality word puzzle game with high quality prefabs, changable parameters through mobile app, the game is for exercise purpose also',
+        prize: '',
+        tags: {
+            types: ['VR Game'],
+            languages: ['c#','css', 'html'],
+            libraries: ['unity networking'],
+            others: ['unity3d']
+        }
+    },
+    {
+        title: 'Java Practice',
+        url: 'https://github.com/harmeet651/JavaPractice',
+        image: 'java.png',
+        description: 'Java programming practice repository',
+        prize: '',
+        tags: {
+            types: [''],
+            languages: ['java'],
+            libraries: [''],
+            others: ['eclipse']
+        }
+    },
+    {
+        title: 'Kinda Chat Bot',
+        url: 'https://github.com/harmeet651/Emotive',
+        image: 'emotive.png',
+        description: 'OPT to a messaging servive and get messages about products',
+        prize: '',
+        tags: {
+            types: ['Web App'],
+            languages: ['html','css', 'javascript'],
+            libraries: ['express','nodejs'],
+            others: ['postgresql']
+        }
+    },
+    {
+        title: 'Alexa App',
+        url: '',
+        image: 'alexa.png',
+        description: 'Alexa skill plays audio\'s between conversations depending upon the conversation',
+        prize: '',
+        tags: {
+            types: ['Alexa App'],
+            languages: ['javascript'],
+            libraries: ['nodejs'],
+            others: ['']
+        }
+    },
+    {
+        title: 'Space Ball',
+        url: 'https://github.com/harmeet651/SpaceBall',
+        image: 'spaceball.png',
+        description: 'Endless runner mobile game which gets difficult with time',
+        prize: '',
+        tags: {
+            types: ['Mobile Game'],
+            languages: ['c#'],
+            libraries: ['unity networking'],
+            others: ['unity3d']
+        }
+    },
  ];
 
  export default projects;
